14e6c50119412f6ee28c7f992b3a1923137da8ee
biau809 <58612152+biau809@users.noreply.github.com>
2024-05-09 14:01:35 +0800

    Update ReportedList.js

--- js/ReportedList.js
+++ js/ReportedList.js
@@ -3,23 +3,27 @@ const url='https://script.google.com/macros/s/AKfycbxpv00JQLhKWnIWzn8QtWX1s0mrjk
 
 
 function QueryString(qStr){
+
     //alert("Enter in QueryString");https://script.google.com/macros/s/AKfycbxpv00JQLhKWnIWzn8QtWX1s0mrjkThBp-Yj2bTJXPLCJKf2V-scVtw-0EJINIA4PJ3Rw/exec
     const val=qStr;
 	//alert(val);
     const url_parameter=`${url}`;
+
     //console.log('click');
     console.log(url_parameter);
     const formData=new FormData();
-
+	
     fetch(url_parameter,{
         method:'POST',
         body:formData
     })
+
     .then(res =>res.json())   // 把request json化, 使用 json() 可以得到 json 物件.then(result => {
     //https://www.youtube.com/watch?v=0No2n9UTxUc
     .then(data=>{
-        const total = Object.keys(data).length;  //計算共有幾筆已填報的資料,https://stackoverflow.com/questions/15209136/how-to-count-length-of-the-json-array-element
+    const total = Object.keys(data).length;  //計算共有幾筆已填報的資料,https://stackoverflow.com/questions/15209136/how-to-count-length-of-the-json-array-element
 	document.getElementById('DateTime').innerHTML = "統計至";
+
     
 	document.getElementById('DateTime').innerHTML += new Date();
 	document.getElementById("ReportList").innerHTML=``;         
